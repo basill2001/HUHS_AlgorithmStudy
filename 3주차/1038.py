f9904888cbbd81856390e49cc5d55595ac982455
basil2001 <copper09ingog@gmail.com>
2023-04-12 12:53:37 +0900

    1038:O

---
+++ 3주차/1038.py
@@ -0,0 +1,18 @@
+from itertools import combinations
+
+N=int(input())
+
+lst=[]
+# 9876543210 -> 10자리 수
+for i in range(1,11):
+    for num in combinations(range(0,10),i):
+        num=sorted(list(num),reverse=True)
+        num="".join(str(digit) for digit in num)
+        lst.append(int(num))
+        
+lst.sort()
+
+if N<len(lst):
+    print(lst[N])
+else:
+    print(-1)
