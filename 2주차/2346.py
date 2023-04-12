22de370bb9be3bf315edabd81d67db762aa84597
basil2001 <copper09ingog@gmail.com>
2023-04-12 14:56:27 +0900

    2346:O

--- 2주차/2346.py
+++ 2주차/2346.py
@@ -10,15 +10,11 @@ balloons=deque(list(i for i in range(1,N+1)))
 popped=[]
 
 for i in range(len(nums)):
-    print("balloons",balloons)
     popped_balloon=balloons[0]
     popped.append(popped_balloon)
     paper=-1*nums[popped[i]-1]
-    print("paper",-paper)
     balloons.rotate(paper)
-    print(balloons)
     balloons.remove(popped_balloon)
-    print(balloons)
 
 for balloon in popped:
     print(balloon,end=" ")
