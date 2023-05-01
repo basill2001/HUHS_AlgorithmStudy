1d9c8c9220b3bb4a0006989dcbca78e8d2627763
basil2001 <copper09ingog@gmail.com>
2023-05-01 12:41:07 +0900

    1427_소트 인사이드 : O

--- 1주차/1427.py
+++ 1주차/1427.py
@@ -1,9 +1,14 @@
+# 입력받기
 N=input()
 digit=len(N)
+
+# 자릿수 별로 분리
 numbers=[]
 for i in range(digit):
     numbers.append(int(N[i]))
+# 정렬
 numbers.sort(reverse=True)
 
+# 출력
 for n in numbers:
     print(n,end="")
