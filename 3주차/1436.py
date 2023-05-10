3be5efaf71ae617c30b95037a8fabd56b2af374e
basil2001 <copper09ingog@gmail.com>
2023-05-10 12:37:28 +0900

    영화감독 숌 : O

--- 3주차/1436.py
+++ 3주차/1436.py
@@ -1,7 +1,10 @@
 N=int(input())
 
-num=0
-count=0
+num=0 # marker
+count=0 # 찾은 종말의 수의 개수
+
+# num을 1씩 증가해가며 종말의 수이면 count를 증가시키기
+# count==N이면 num은 N번째 종말의 수
 while count<N:
     num+=1
     if "666" in str(num):
