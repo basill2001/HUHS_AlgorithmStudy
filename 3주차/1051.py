94c67d6148f26bee6b502f6e2763ae3830712ebf
basil2001 <copper09ingog@gmail.com>
2023-05-10 12:35:07 +0900

    숫자 정사각형 : O

--- 3주차/1051.py
+++ 3주차/1051.py
@@ -1,24 +1,24 @@
+# N,M 크기의 직사각형 받기
 N,M=map(int,input().split())
 rectangle=[]
 
 for i in range(N):
     rectangle.append(input())
 
-sq=min(N,M)
-horizon=False
-if N==sq:
-    horizon=True
+# 정사각형의 최대 크기를 더 짧은 변의 길이로
+sq=min(N,M) 
 
-def check(rec,size):
+# 한 변이 size인 정사각형의 꼭짓점에 쓰여있는 수가 모두 같은지
+def check(size):
     size=size-1
     for i in range(N-(size)):
         for j in range(M-(size)):
-            if rec[i][j]==rec[i][j+size]==rec[i+size][j+size]==rec[i+size][j]:
+            if rectangle[i][j]==rectangle[i][j+size]==rectangle[i+size][j+size]==rectangle[i+size][j]:
                 return True
             else:
                 continue             
 
-for size in range(sq,0,-1):
-    if check(rectangle,size)==True:
+for size in range(sq,0,-1): # size를 역순으로
+    if check(size)==True:
         print(size**2)
         break
