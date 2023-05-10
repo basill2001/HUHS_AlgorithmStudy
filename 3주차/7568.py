6e3dfc9337db2e89b15fab5bcfa6e088b03e8a81
basil2001 <copper09ingog@gmail.com>
2023-05-10 14:03:38 +0900

    덩치 : O

--- 3주차/7568.py
+++ 3주차/7568.py
@@ -1,6 +1,7 @@
 import sys
 input=sys.stdin.readline
 
+# 사람 입력받기
 N=int(input())
 lst=[]
 for i in range(N):
@@ -8,13 +9,16 @@ for i in range(N):
     lst.append((x,y))
 
 order=[]
+# 각 사람마다
 for i in range(N):
+    # 덩치가 더 큰 사람 명수
     bigger=0
     for j in range(N):
         if lst[i][0]<lst[j][0] and lst[i][1]<lst[j][1]:
             bigger+=1
     order.append(bigger+1)
 
+# 출력
 for big in order:
     print(big,end=" ")
         
